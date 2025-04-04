5f10ddb910f9b964a14fb4d7397cdcd92e1af357
Jiwei Cao <jiweicao2006@gmail.com>
2025-04-04 20:23:54 +0100

    Handling Data

--- Cryptocurrecy Price Forecasting.py
+++ Cryptocurrecy Price Forecasting.py
@@ -8,3 +8,25 @@ from copy import deepcopy as dc
 from sklearn.preprocessing import MinMaxScaler
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Opening and handling the data
+data = pd.read_csv('Bitcoin_04_04_2024-04_04_2025_historical_data_coinmarketcap.csv', sep=';')
+data = data[['timeOpen', 'close']]
+data['timeOpen'] = pd.to_datetime(data['timeOpen'])
+#plt.plot(data['timeOpen'], data['close'])
+
+def prepare_dataframe_for_lstm(df, n_steps):
+  # Create a deepcopy of the dataframe
+  df = dc(df)
+
+  # Set the index of the dataframe to the date
+  df.set_index('timeOpen', inplace=True)
+
+  # Shifts the dataframe for the number of lookback windows (n_steps)
+  for i in range(1, n_steps+1):
+    df[f'Close(t-{i})'] = df['close'].shift(i)
+
+  # Removes any missing values from the dataframe such as NaN
+  df.dropna(inplace=True)
+
+  return df
